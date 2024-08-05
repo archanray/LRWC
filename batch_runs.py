035e8f0a64b557ed202120a08a7feecc1e39a91c
Ray <raya2@mskcc.org>
2024-08-05 15:19:23 -0400

    automated system commit

--- batch_runs.py
+++ batch_runs.py
@@ -1,8 +1,8 @@
 import os
 
 for i in range(1,6):
-    #command = "python main.py --method modifiedBKKS21 --patient Paraspinal_Patient_2 --threshold 0.05 --solver MOSEK 2>&1 | tee logs/RMR_PS2_005_"+str(i)+".txt"
-    command = "python main.py --method modifiedBKKS21 --patient Paraspinal_Patient_2 --samples 3500000 --solver MOSEK 2>&1 | tee logs/mBSSK21_PS2_3500000_"+str(i)+".txt"
+    command = "python main.py --method modifiedBKKS21 --patient Paraspinal_Patient_2 --threshold 0.05 --solver MOSEK 2>&1 | tee logs/RMR_PS2_005_"+str(i)+".txt"
+    # command = "python main.py --method modifiedBKKS21 --patient Paraspinal_Patient_2 --samples 3500000 --solver MOSEK 2>&1 | tee logs/mBSSK21_PS2_3500000_"+str(i)+".txt"
     os.system(command)
     
 os.system("git add .")
